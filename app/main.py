204944a69718f2b50abd1f509ddded88f96c3f24
BapanBigData <bapanmldl7892@gmail.com>
2025-05-06 02:01:08 +0530

    pushed

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
+import os
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from app.server.websocket_server import router as websocket_router
 
 
 app = FastAPI()
+
+
+client = os.path.join(os.path.dirname(__file__), "client", "index.html")
+
+
+@app.get("/")
+async def get_index():
+    return FileResponse(client)
+
 
 app.include_router(websocket_router)
--- app/server/websocket_server.py
+++ app/server/websocket_server.py
@@ -6,16 +6,16 @@ router = APIRouter()
 manager = ConnectionManager()
 
 
-@router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+@router.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     
     try:
         while True:
             data = await websocket.receive_text()
             await manager.send_personal_message(f"You said: {data}", websocket)
-            await manager.broadcast(f"{data}")
+            await manager.broadcast(f"Client #{client_id} says: {data}")
             
     except WebSocketDisconnect:
         manager.disconnect(websocket)
-        # await manager.broadcast("A user disconnected")
+        await manager.broadcast(f"Client #{client_id} has left the chat")
